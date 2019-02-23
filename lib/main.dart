25d5da2de90238d7530adfb4a03043b12abec502
shater <mohamed.zakria.shater@gmail.com>
2019-02-23 02:12:16 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+main() {
+  var myApp = new MaterialApp(
+    title: "new materia app",
+    home: new Text("this is new mateial app"),
+  );
+  runApp(myApp);
+}
